f1bf71791992bdd24443c425d6c8a8053f03268e
Jerry Dumas <dumas.jerry@gmail.com>
2022-02-25 23:16:58 -0500

    Goals controller and routes setup

---
+++ backend/routes/goalRoutes.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const router = express.Router()
+const { getGoals, setGoal, updateGoal, deleteGoal } = require('../controllers/goalController')
+    //make request
+    // router.get('/', getGoals)
+    // router.post('/', setGoal)
+    // router.put('/:id', updateGoal)
+    // router.delete('/:id', deleteGoal)
+router.route('/').get(getGoals).post(setGoal)
+router.route('/:id').delete(deleteGoal).put(updateGoal)
+module.exports = router
--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,21 @@
-console.log('Hello World');
+//declare variable / import dependancies
+//web application framework
+const express = require('express')
+    //Loads environment variables from .env file
+const dotenv = require('dotenv').config()
+    //bring in error handler
+const { errorHandler } = require('./middleware/errorMiddleware')
+    //declare port variable
+    //port is equal to environmental variable or assigned port
+const port = process.env.PORT || 5000
+
+//declare app 
+const app = express()
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+    //require routes
+app.use('/api/goals', require('./routes/goalRoutes'))
+    //listen for port
+app.listen(port, () => {
+    console.log(`Server started on port ${port}`);
+})
